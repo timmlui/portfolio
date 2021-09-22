af1e9b724c08f06b55aa97e414ea728556134488
Timothy Lui <timothy.lui@publicissapient.com>
2021-09-22 09:57:05 -0400

    stroke animation for name, infinite horizontal scrolling, spotlight invert for finding

--- src/App.js
+++ src/App.js
@@ -1,128 +1,153 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
-import About from './components/About';
-import Education from './components/Education';
-import WorkExperience from './components/WorkExperience';
-import Project from './components/Project';
+import Slide from './components/Slide';
 
-import './App.css';
-import illustration from './assets/img/computer-typing.gif';
+import { sortColors } from './sortColors';
+
+import './App.scss';
 
 function App() {
   const [data, setData] = useState({});
+  const [svgs, setSvgs] = useState({});
+  const [intersect, setIntersect] = useState(false);
+  const pathEl = useRef(null);
+  const cursor = useRef(null);
+  const findMe = useRef(null);
   
   useEffect(() => {
     const data = require('./data.json');
     setData(data);
+    const svgs = require('./svg.json');
+    setSvgs(svgs);
+
+    _handleAnimation();
+
+    // Event Listeners
+
+    document.body.onmousemove = function(e) {
+      document.documentElement.style.setProperty('--x', (e.clientX+window.scrollX) + 'px');
+      document.documentElement.style.setProperty('--y', (e.clientY+window.scrollY) + 'px');
+    }
+
+    document.addEventListener('mousemove', _showSpotlight);
+
+    document.documentElement.style.setProperty('--scale', 0);
   }, []);
 
-  function renderAbout() {
-    const { about = {}, workExperiences = [] } = data;
+  useEffect(() => {
+    if (intersect) {
+      console.log('is intersectping')
+      // Needed to prevent the shrink animation on refresh.
+      document.documentElement.style.setProperty('--scale', 150);
+      cursor.current.classList.remove('inverted-inactive');
+      cursor.current.classList.add('inverted-active');
+    } else {
+      console.log('NOT intersectping')
+      cursor.current.classList.remove('inverted-active');
+      cursor.current.classList.add('inverted-inactive');
+    }
+  }, [intersect])
 
-    return (
-      <About 
-        {...about}
-        latestWorkExperience = {workExperiences.length && workExperiences[0]}
-      />
-    )
+  const _showSpotlight = () => {
+    const el1 = findMe.current.getBoundingClientRect();
+    const el2 = cursor.current.getBoundingClientRect();
+
+    const _elementIntersect = (el1, el2) => {
+      return !(el1.right < el2.left || 
+        el1.left > el2.right || 
+        el1.bottom < el2.top || 
+        el1.top > el2.bottom);
+    }
+
+    const isIntersect = _elementIntersect(el1, el2);
+
+    if (isIntersect && !intersect) {
+      setIntersect(true);
+    } else if (!isIntersect && !intersect) {
+      setIntersect(false);
+    }
   }
 
-  function renderEducation() {
-    const { education = [] } = data;
+  const _renderSlides = () => {
+    const { skills = [] } = svgs;
 
-    const _educations = education.map((education) => (
-      <Education {...education} />
+    // Sort the colors of the icons by HUE.
+    if (skills.length > 0) {
+      sortColors(skills);
+    }
+
+    const slides = skills.map((svg) => (
+      <Slide {...svg} />
     ));
+
+    // Needed for the illusion of an infinite scroll
+    const duplicated = [...slides, ...slides];
 
     return (
       <React.Fragment>
-        <p class="section-tagline" style={{ textAlign: 'left' }}>Education</p>
-        {_educations}
+        {duplicated}
       </React.Fragment>
     )
   }
 
-  function renderWorkExperiences() {
-    const { workExperiences = [] } = data;
+  const _handleAnimation = () => {
+    const path = pathEl.current;
+    const length = path.getTotalLength();
 
-    const _workExperiences = workExperiences.map((workExperience) => (
-      <WorkExperience {...workExperience} />
-    ));
-
-    return (
-      <React.Fragment>
-        <p class="section-tagline" style={{ textAlign: 'left' }}>Work Experience</p>
-        {_workExperiences}
-      </React.Fragment>
-    )
-  }
-
-  function renderProjects() {
-    const { projects = [] } = data;
-
-    const _projects = projects.map((project) => (
-      <Project {...project} />
-    ));
-
-    return (
-      <React.Fragment>
-        <p class="tagline">Work + Fun</p>
-        {_projects}
-      </React.Fragment>
-    )
+    // Clear any previous transition
+    path.style.transition = path.style.WebkitTransition = 'none';
+    // Set up the starting positions
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    // Trigger a layout so styles are calculated & the browser
+    // picks up the starting position before animating
+    path.getBoundingClientRect();
+    // Define our transition
+    path.style.transition = path.style.WebkitTransition =
+      'stroke-dashoffset 2s ease-in-out';
+    // Go!
+    path.style.strokeDashoffset = '0';
   }
 
   return (
-    <div class="app-wrapper">
-      <div id="toc-holder" class="toc-holder">
-        <a href="#" class="toc-link" id="toc-link"><span class="">☰</span></a>
-        <ul id="toc" class="toc toc-up" style={{ top: '-310px' }}>
-          <li class="toc-h1"><a href="#about">About</a></li>
-          <li class="toc-h1"><a href="#resume">Résumé</a></li>
-          <li class="toc-h1"><a href="#work">Work</a></li>
-        </ul>
+    <div class="container">
+      <div class="header-nav">
+        <a class="logo" href="/#">TL.</a>
+        <a class="resume-link" href="/resume">Resume</a>
       </div>
-
-      <div id="page-wrapper">
-        <div id="header">
-          <div class="inner name"><a href="#illustration">Timothy Lui</a></div>
-          <div class="nav">
-          <ul class="mobile-nav">
-          <li class="mobile-header"><a href="#about">About</a></li>
-          <li class="mobile-header"><a href="#resume">Resumé</a></li>
-          <li class="mobile-header"><a href="#work">Work</a></li></ul>
-          </div>
-        </div>
-        <div id="illustration"><center><img src={illustration}/></center></div>
-        
-        {renderAbout()}
-
-        <div id="resume">
-          <div class="inner">
-            <p class="tagline">Résumé</p>
-
-            {renderWorkExperiences()}
-
-            {renderEducation()}
-          </div>
-        </div>
-        {/* <!-- Portfolio --> */}
-        <div id="work">
-          <div class="inner">
-            {renderProjects()}
+      <div class="name-plate">
+        <svg id="name-svg" width="200" height="100" viewBox="0 0 600 100" overflow="visible">
+        <path
+          stroke="#000"
+          stroke-width="10"
+          fill="none"
+          stroke-dasharray="988.00 988.00"
+          stroke-dashoffset="0.00"
+          d="M62.9 14.9c-25-7.74-56.6 4.8-60.4 24.3-3.73 19.6 21.6 35 39.6 37.6 42.8 6.2 72.9-53.4 116-58.9 65-18.2 191 101 215 28.8 5-16.7-7-49.1-34-44-34 11.5-31 46.5-14 69.3 9.38 12.6 24.2 20.6 39.8 22.9 91.4 9.05 102-98.9 176-86.7 18.8 3.81 33 17.3 36.7 34.6 2.01 10.2.124 21.1-5.18 30.1"
+          ref={pathEl}
+        />
+        </svg>
+      </div>
+      <div class="technicals">
+        <span>github link</span>
+        <div class="slider">
+          <div class="slider-track">
+            {_renderSlides()}
           </div>
         </div>
       </div>
-      			
-      <div id="footer"><div class="inner">
-        © 2020 Timothy Lui. 
-        <span class="right">
-          <a target="_blank" href="https://github.com/timmlui">Github</a>&nbsp;//&nbsp;
-          <a target="_blank" href="mailto:lui.timm@gmail.com">Email</a>&nbsp;//&nbsp;
-          <a target="_blank" href="https://www.linkedin.com/in/timothy-lui-1b8638170/">LinkedIn</a>
+      <div class="cursor" ref={cursor}></div>
+      <div class="find-me" ref={findMe}>
+        <span class="find-me_title">
+          FIND ME
         </span>
-      </div></div>
+        <span class="find-me_details">
+          <a href="lui.timm@gmail.com">lui.timm@gmail.com</a>
+          <span>github link</span>
+          <span>phone num</span>
+        </span>
+      </div>
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header(props) {
+
+  return (
+    <div>
+      Timothy
+    </div>
+  )
+}
+
+export default Header;
