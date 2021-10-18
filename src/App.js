7ae505363732c65e6d352857b2a637f52e2fc794
Timothy Lui <timothy.lui@publicissapient.com>
2021-10-17 22:11:15 -0400

    spotlight reveals hidden text with hover detection to not invert colours. name plate svg per letter and animated on fresh load - determines length of each svg path (letter) dynamically, allowing different svgs to be switched easily.

--- src/App.js
+++ src/App.js
@@ -2,63 +2,53 @@
 import React, { useEffect, useState, useRef } from 'react';
 
 import Slide from './components/Slide';
+import NamePlateSVG from './components/NamePlateSVG';
 
 import { sortColors } from './sortColors';
 
 import './App.scss';
+
+import black from "./assets/img/black.jpeg";
 
 function App() {
   const [data, setData] = useState({});
   const [svgs, setSvgs] = useState({});
   const [intersect, setIntersect] = useState(false);
   const namePlate = useRef(null);
-  const pathEl = useRef(null);
+  // const pathEl = useRef(null);
   const cursor = useRef(null);
   const findMe = useRef(null);
   const findMeTitle = useRef(null);
+  const findMeDetails = useRef(null);
   
+
+  /**
+   * On Mount events to run. Pulling JSON data, eventlisteners, initial setups.
+   */
   useEffect(() => {
     const data = require('./data.json');
     setData(data);
     const svgs = require('./svg.json');
     setSvgs(svgs);
 
-    _handleAnimation();
-
-    // Event Listeners
+    // Event Listeners - mouse movement.
     document.addEventListener('mousemove', _mouseTrack);
     document.addEventListener('scroll', _showSpotlight);
 
-    // window.addEventListener('scroll', () => {
-  
-    //   if(window.scrollY + window.innerHeight > document - 100) {
-    //       findMeTitle.current.classList().add('show');
-    //   } else if(findMeTitle.current.classList.contains('show') && window.scrollY + window.innerHeight > document.height - 150) {
-    //       findMeTitle.current.classList.remove('show');
-    //   }
-    // });
+    // Reset to top of the page upon reload.
     window.addEventListener('beforeunload', () => {
-      console.log('cleanup')
       namePlate.current.scrollIntoView();
     });
 
-    const options = {
-      threshold: 0
-    }
+    // _handleAnimation();
+    const name = document.querySelectorAll('#name-svg path');
+    name.forEach((letter, index) => {
+      console.log(index, letter.getTotalLength(), letter.style);
+      letter.style.strokeDasharray = letter.getTotalLength();
+      letter.style.strokeDashoffset = letter.getTotalLength();
+    });
 
-    const target = findMe.current;
-
-    const intersectionCallback = (entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          let elem = entry.target;
-          console.log('intersecting', elem)
-        }
-      });
-    }
-
-    let observer = new IntersectionObserver(intersectionCallback, options);
-    observer.observe(target);
+    _handleParallax();
   }, []);
 
   useEffect(() => {
@@ -68,36 +58,62 @@ function App() {
       cursor.current.classList.add('inverted-active');
     } else {
       // console.log('NOT intersectping')
-      if (Array.from(cursor.current.classList).includes('inverted-active')) {
+      if (cursor.current.classList.contains('inverted-active')) {
         cursor.current.classList.remove('inverted-active');
         cursor.current.classList.add('inverted-inactive');
       }
     }
   }, [intersect])
 
+  /**
+   * Sets the coordinates of the spotlight to match the cursor.
+   * @param {*} e - event from mousemove
+   */
   const _mouseTrack = (e) => {
     cursor.current.style.left = e.clientX - cursor.current.offsetWidth / 2 + "px";
     cursor.current.style.top = e.clientY - cursor.current.offsetHeight / 2 + "px";
     _showSpotlight();
   }
 
+  /**
+   * Determine if el1 and el2 are intersecting.
+   * @param {*} el1 - first element
+   * @param {*} el2 - second element
+   */
+  const _elementIntersect = (el1, el2) => {
+    return !(el1.right < el2.left || 
+      el1.left > el2.right || 
+      el1.bottom < el2.top || 
+      el1.top > el2.bottom);
+  }
+
+  /**
+   * Event mousemove to track when cursor is in the "Find Me" section to show
+   * the spotlight. Handles the mix-blend-mode of the spotlight when needed for 
+   * the hidden content.
+   */
   const _showSpotlight = () => {
     const el1 = findMe.current.getBoundingClientRect();
     const el2 = cursor.current.getBoundingClientRect();
+    const el3 = findMeDetails.current.getBoundingClientRect();
 
-    const _elementIntersect = (el1, el2) => {
-      return !(el1.right < el2.left || 
-        el1.left > el2.right || 
-        el1.bottom < el2.top || 
-        el1.top > el2.bottom);
+    const isIntersectSection = _elementIntersect(el1, el2);
+    const isIntersectDetails = _elementIntersect(el2, el3);
+
+    // Handle when cursor is within section.
+    if (isIntersectSection && !intersect) {
+      setIntersect(true);
+    } else if (!isIntersectSection && !intersect) {
+      setIntersect(false);
     }
 
-    const isIntersect = _elementIntersect(el1, el2);
-
-    if (isIntersect && !intersect) {
-      setIntersect(true);
-    } else if (!isIntersect && !intersect) {
-      setIntersect(false);
+    // To handle the hidden content to show when spotlight hovers over.
+    if (isIntersectDetails) {
+      cursor.current.style.mixBlendMode = "normal";
+      cursor.current.style.zIndex = 0;
+    } else {
+      cursor.current.style.mixBlendMode = "difference";
+      cursor.current.style.zIndex = 9;
     }
   }
 
@@ -123,23 +139,57 @@ function App() {
     )
   }
 
-  const _handleAnimation = () => {
-    const path = pathEl.current;
-    const length = path.getTotalLength();
+  /**
+   * Initial idea to use a single svg path for cursive written text for name plate.
+   */
+  // const _handleAnimation = () => {
+  //   const path = pathEl.current;
+  //   const length = path.getTotalLength();
 
-    // Clear any previous transition
-    path.style.transition = path.style.WebkitTransition = 'none';
-    // Set up the starting positions
-    path.style.strokeDasharray = length + ' ' + length;
-    path.style.strokeDashoffset = length;
-    // Trigger a layout so styles are calculated & the browser
-    // picks up the starting position before animating
-    path.getBoundingClientRect();
-    // Define our transition
-    path.style.transition = path.style.WebkitTransition =
-      'stroke-dashoffset 2s ease-in-out';
-    // Go!
-    path.style.strokeDashoffset = '0';
+  //   // Clear any previous transition
+  //   path.style.transition = path.style.WebkitTransition = 'none';
+  //   // Set up the starting positions
+  //   path.style.strokeDasharray = length + ' ' + length;
+  //   path.style.strokeDashoffset = length;
+  //   // Trigger a layout so styles are calculated & the browser
+  //   // picks up the starting position before animating
+  //   path.getBoundingClientRect();
+  //   // Define our transition
+  //   path.style.transition = path.style.WebkitTransition =
+  //     'stroke-dashoffset 2s ease-in-out';
+  //   // Go!
+  //   path.style.strokeDashoffset = '0';
+  // }
+
+  /**
+   * Parallax effect for when the Find-Me section is visible.
+   * Slides the header up when visibility threshold reaches 80%.
+   */
+  const _handleParallax = () => {
+    const options = {
+      threshold: 0.8
+    }
+
+    const target = findMe.current;
+
+    const intersectionCallback = (entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          console.log('intersecting find-me')
+          findMeTitle.current.classList.remove('hide');
+          findMeTitle.current.classList.add('show');
+        } else {
+          console.log('NOT intersecting find-me')
+          if (findMeTitle.current.classList.contains('show')) {
+            findMeTitle.current.classList.remove('show');
+            findMeTitle.current.classList.add('hide');
+          }
+        }
+      });
+    }
+
+    let observer = new IntersectionObserver(intersectionCallback, options);
+    observer.observe(target);
   }
 
   return (
@@ -151,19 +201,7 @@ function App() {
           <a class="resume-link" href="/resume">Resume</a>
         </div>
       </div>
-      <div class="name-plate fixed" ref={namePlate}>
-        <svg id="name-svg" width="200" height="100" viewBox="0 0 600 100" overflow="visible">
-          <path
-            stroke="#fff"
-            stroke-width="10"
-            fill="none"
-            stroke-dasharray="988.00 988.00"
-            stroke-dashoffset="0.00"
-            d="M62.9 14.9c-25-7.74-56.6 4.8-60.4 24.3-3.73 19.6 21.6 35 39.6 37.6 42.8 6.2 72.9-53.4 116-58.9 65-18.2 191 101 215 28.8 5-16.7-7-49.1-34-44-34 11.5-31 46.5-14 69.3 9.38 12.6 24.2 20.6 39.8 22.9 91.4 9.05 102-98.9 176-86.7 18.8 3.81 33 17.3 36.7 34.6 2.01 10.2.124 21.1-5.18 30.1"
-            ref={pathEl}
-          />
-        </svg>
-      </div>
+      <NamePlateSVG ref={namePlate}></NamePlateSVG>
       <div class="technicals">
         <div class="technicals_title">
           <span>github link</span>
@@ -178,14 +216,17 @@ function App() {
       </div>
       <div class="cursor" ref={cursor}></div>
       <div class="find-me" ref={findMe}>
-        <span class="find-me_title" ref={findMeTitle}>
-          FIND ME
-        </span>
-        <span class="find-me_details">
-          <a href="lui.timm@gmail.com">lui.timm@gmail.com</a>
-          <span>github link</span>
-          <span>phone num</span>
-        </span>
+        <div class="find-me_inner">
+          <span class="find-me_title" ref={findMeTitle}>
+            FIND ME
+          </span>
+          <span class="find-me_details" ref={findMeDetails}>
+            <a href="lui.timm@gmail.com">lui.timm@gmail.com</a>
+            <span>github link</span>
+            <span>phone num</span>
+            <img src={black} width="100" height="100" alt="asdf" />
+          </span>
+        </div>
       </div>
     </div>
   )
