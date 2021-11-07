a01e30ef4d428ec83513e2a887d7364209e2d15b
Timothy Lui <timothy.lui@publicissapient.com>
2021-11-07 17:42:06 -0500

    Updates

---
+++ precache-manifest.b9245169c9ea80fd8ed07bf0fcb27f52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4866efc09f09ff3c387015af8a28c7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a390e73774845a9c67d2",
+    "url": "/portfolio/static/css/main.1db4efe5.chunk.css"
+  },
+  {
+    "revision": "38c2155103918f173274",
+    "url": "/portfolio/static/js/2.9ac9c9c1.chunk.js"
+  },
+  {
+    "revision": "38d5cc1cb1001b9cc38b06e2da080c18",
+    "url": "/portfolio/static/js/2.9ac9c9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a390e73774845a9c67d2",
+    "url": "/portfolio/static/js/main.876acba0.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "ab7292669401544e726eab12da07f1f1",
+    "url": "/portfolio/static/media/Timothy_Lui_Resume_2021.ab729266.pdf"
+  }
+]);
